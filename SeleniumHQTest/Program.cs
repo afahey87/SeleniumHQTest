eab1303cfebf8f720935ab87a212e07a89ce5e8e
Fahey <adam.fahey@tylertech.com>
2017-10-09 21:09:12 -0400

    adding drivers

---
+++ SeleniumHQTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumHQTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We are going to run an automated test at http://www.seleniumhq.org/ ");
+        }
+    }
+}
---
+++ SeleniumHQTest/Test.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+
+namespace Alerts_and_Windows
+{
+
+    [TestFixture]
+    class SeleniumHQTest
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.seleniumhq.org/");
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+        }
+
+
+        public IWebElement waitForPageUntillElementIsVissable(By locator, int maxseconds)
+        {
+            return new WebDriverWait(driver, TimeSpan.FromSeconds(maxseconds))
+                .Until(ExpectedConditions.ElementExists((locator)));
+        }
+
+        public void waitOnPage(int seconds)
+        {
+            System.Threading.Thread.Sleep(seconds * 1000);
+        }
+
+
+        [TearDown]
+        public void teardown()
+        {
+            //driver.Close();
+           // driver.Quit();
+        }
+      
+        [Test]
+        public void SeleniumHQTest()
+        {
+            driver.FindElement(By.XPath("/html/body/div[1]/div[1]/ul/li[5]/a")).Click();
+            driver.findElement(By.XPath("/html/body/div/div[2]/div[2]/div/h3[1]/a")).Click();
+
+            
+        }
+
+        
+
+    }
+}
